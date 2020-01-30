82712fa6726dd1c4738f8e0ad704c6b53939fae2
unknown <Station@r116-10.ad.osnabrueck-bbs-brinkstr.logodidact.net>
2020-01-30 14:29:35 +0100

    Add finance and management component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FinanceComponent } from './finance/finance.component';
+import { MemberManagementComponent } from './member-management/member-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinanceComponent,
+    MemberManagementComponent
   ],
   imports: [
     BrowserModule,
